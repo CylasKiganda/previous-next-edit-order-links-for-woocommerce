1a945d2bcaf66de48eb511ac8d31907408a06f28
Cylas Kiganda <kigandacylas@gmail.com>
2023-03-03 23:04:46 +0300

    woocommerce check added

--- previous-next-edit-order-links-for-woocommerce.php
+++ previous-next-edit-order-links-for-woocommerce.php
@@ -30,6 +30,22 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+$acrw_active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins' ) );
+if ( is_multisite() ){
+	$acrw_active_plugins = array_merge( $acrw_active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
+
+}
+	
+$acrw_required_plugins = array(
+  "woocommerce/woocommerce.php"
+);
+
+foreach ($acrw_required_plugins as $rp) {
+    if ( ! in_array( $rp, $acrw_active_plugins ) ) {
+        return;
+    }
+}
+
 /**
  * Currently plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
